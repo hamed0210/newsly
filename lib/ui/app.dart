a5f1f5592eb0745dd8c1bcf1e9db690525470847
hduran10 <hduran0210@gmail.com>
2021-12-01 00:08:21 -0500

    Inicio de proyecto vacio

---
+++ lib/ui/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+      ),
+    );
+  }
+}
